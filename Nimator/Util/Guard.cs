1d6e99d878c8dcb65bc074b2a013a6b217d7b705
Fred Kleuver <fred@avurad.nl>
2018-04-07 08:59:28 +0200

    feat(guard): add value returning guards

--- Nimator/Util/Guard.cs
+++ Nimator/Util/Guard.cs
@@ -15,12 +15,32 @@ namespace Nimator.Util
         }
 
         [ContractAnnotation("value: null => halt")]
+        public static T AgainstNull_Return<T>([InvokerParameterName] string argumentName, [NotNull] T value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+            return value;
+        }
+
+        [ContractAnnotation("value: null => halt")]
         public static void AgainstNullAndEmpty([InvokerParameterName] string argumentName, [NotNull] string value)
         {
             if (string.IsNullOrWhiteSpace(value))
             {
                 throw new ArgumentNullException(argumentName);
             }
+        }
+
+        [ContractAnnotation("value: null => halt")]
+        public static string AgainstNullAndEmpty_Return([InvokerParameterName] string argumentName, [NotNull] string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+            return value;
         }
 
         [ContractAnnotation("value: null => halt")]
@@ -36,12 +56,35 @@ namespace Nimator.Util
             }
         }
 
+        [ContractAnnotation("value: null => halt")]
+        public static T AgainstNullAndEmpty_Return<T>([InvokerParameterName] string argumentName, [NotNull, NoEnumeration] T value) where T : ICollection
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+            if (value.Count == 0)
+            {
+                throw new ArgumentOutOfRangeException(argumentName);
+            }
+            return value;
+        }
+
         public static void AgainstNegativeAndZero([InvokerParameterName] string argumentName, int value)
         {
             if (value <= 0)
             {
                 throw new ArgumentOutOfRangeException(argumentName);
             }
+        }
+
+        public static int AgainstNegativeAndZero_Return([InvokerParameterName] string argumentName, int value)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentOutOfRangeException(argumentName);
+            }
+            return value;
         }
 
         public static void AgainstNegative([InvokerParameterName] string argumentName, int value)
@@ -52,6 +95,15 @@ namespace Nimator.Util
             }
         }
 
+        public static int AgainstNegative_Return([InvokerParameterName] string argumentName, int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException(argumentName);
+            }
+            return value;
+        }
+
         public static void AgainstNegativeAndZero([InvokerParameterName] string argumentName, TimeSpan value)
         {
             if (value <= TimeSpan.Zero)
@@ -60,12 +112,30 @@ namespace Nimator.Util
             }
         }
 
+        public static TimeSpan AgainstNegativeAndZero_Return([InvokerParameterName] string argumentName, TimeSpan value)
+        {
+            if (value <= TimeSpan.Zero)
+            {
+                throw new ArgumentOutOfRangeException(argumentName);
+            }
+            return value;
+        }
+
         public static void AgainstNegative([InvokerParameterName] string argumentName, TimeSpan value)
         {
             if (value < TimeSpan.Zero)
             {
                 throw new ArgumentOutOfRangeException(argumentName);
             }
+        }
+
+        public static TimeSpan AgainstNegative_Return([InvokerParameterName] string argumentName, TimeSpan value)
+        {
+            if (value < TimeSpan.Zero)
+            {
+                throw new ArgumentOutOfRangeException(argumentName);
+            }
+            return value;
         }
     }
 }
